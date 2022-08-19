7fa08df72dd4c322e34a92b540e9261060ad0c20
H. Istvan <66251121+steinily@users.noreply.github.com>
2022-08-19 12:51:32 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const bakeryData = require('./src/data/bakery.json')
+
+
+const recipies = Object.entries(bakeryData.recipies)
+
+console.log(recepies) 
+//          Challenge  1 
+// What was last week's sales in terms of money?
+
+// With the Tax Authority already breathing down Stevie's neck, he quickly needs to report his last week of sales. Based on the data you received, calculate how much money he generated, if he only needs to report his total sales.
+
+//Your function should take the whole data-set, and return a number, calculated based on his last week's sales, and the price he sells the sweets for.
+
