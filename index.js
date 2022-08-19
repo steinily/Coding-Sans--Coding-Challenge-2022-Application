138dae7834ce5516ad68ffe29891b4b718c0bc7f
steinily <henczistvan87@gmail.com>
2022-08-19 17:04:35 +0200

    challange-one done

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 const bakeryData = require('./src/data/bakery.json')
+const incomeLastWeek = require('./src/challanges/chalanges-one')
 
-
-const recipies = Object.entries(bakeryData.recipies)
-
-console.log(recepies) 
-//          Challenge  1 
-// What was last week's sales in terms of money?
-
-// With the Tax Authority already breathing down Stevie's neck, he quickly needs to report his last week of sales. Based on the data you received, calculate how much money he generated, if he only needs to report his total sales.
-
-//Your function should take the whole data-set, and return a number, calculated based on his last week's sales, and the price he sells the sweets for.
-
+console.log(incomeLastWeek(bakeryData))
---
+++ src/challanges/chalanges-one.js
@@ -0,0 +1,62 @@
+//------------------------------------------------------------------------------------
+//          Challenge  1
+// What was last week's sales in terms of money?
+
+// With the Tax Authority already breathing down Stevie's neck,
+// he quickly needs to report his last week of sales. Based on the data you received,
+// calculate how much money he generated, if he only needs to report his total sales.
+
+//Your function should take the whole data-set, and return a number,
+// calculated based on his last week's sales, and the price he sells the sweets for.
+
+//------------------------------------------------------------------------------------
+
+/**
+ * It takes an object with a name and amount property, finds the price of the item with the same name,
+ * and returns the product of the price and amount
+ * @param data - an object with two properties:
+ * @returns The total income of the bakery for the week.
+ */
+
+function salesOfLastWeek(data) {
+  /* Taking the data from the recipes object and mapping it to a new array. It is then taking the
+    name and price from the object and assigning it to a new object. It is then taking the price and
+    converting it to a number. */
+  const bakeryPrices = Object.values(data.recipes).map((entrys) => {
+    const { name, price } = entrys;
+    const priceNum = Number(price.replace(/[^0-9\.-]+/g, ""));
+    return {
+      name,
+      priceNum,
+    };
+  });
+
+  /* Taking the data from the salesOfLastWeek object and mapping it to a new array. It is then taking the
+    name and amount from the object and assigning it to a new object. */
+  const sales = Object.values(data.salesOfLastWeek).map((entrys) => {
+    const { name, amount } = entrys;
+    return {
+      name,
+      amount,
+    };
+  });
+
+  /**
+   * It takes an object with a name and amount property, finds the price of the item with the same name,
+   * and returns the product of the price and amount
+   * @param salesItem - the object that is being passed in from the sales array
+   * @returns The price of the item multiplied by the amount of the item.
+   */
+  let income = (salesItem) => {
+    const price = bakeryPrices.find(({ name }) => name === salesItem.name);
+    return price.priceNum * salesItem.amount;
+  };
+
+  let initValue = 0;
+  const sumSales = sales.forEach((element) => {
+    initValue += income(element);
+  });
+  return initValue;
+}
+
+module.exports=salesOfLastWeek;
